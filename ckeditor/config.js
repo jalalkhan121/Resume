3c6ccc6d71694a5bf9ad3b58271e4eeb6225a135
Jalal Khan <jalalkhan121@gmail.com>
2025-12-27 12:51:57 +0500

    CK Editor

---
+++ ckeditor/config.js
@@ -0,0 +1,27 @@
+﻿/*
+
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+
+For licensing, see LICENSE.html or http://ckeditor.com/license
+
+*/
+
+
+
+CKEDITOR.editorConfig = function( config )
+
+{
+
+	// Define changes to default configuration here. For example:
+	config.extraPlugins = 'urdu';
+	config.height = '300';
+	config.width = '600';
+
+	config.enterMode = CKEDITOR.ENTER_BR;
+
+	// config.language = 'fr';
+
+	// config.uiColor = '#AADC6E';
+
+};
+
